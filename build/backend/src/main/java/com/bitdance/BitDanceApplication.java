f4354514d2005fbd7ef75afc6ab4b815c862a94a
ABLT1273 <737044355@qq.com>
2026-05-17 12:03:24 +0800

    代理屏蔽：现在启动不会受代理影响了

--- build/backend/src/main/java/com/bitdance/BitDanceApplication.java
+++ build/backend/src/main/java/com/bitdance/BitDanceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+
 @SpringBootApplication
 @EnableScheduling
 public class BitDanceApplication {
     public static void main(String[] args) {
+        System.setProperty("socksNonProxyHosts", "98.142.241.155|localhost|127.0.0.1");
         SpringApplication.run(BitDanceApplication.class, args);
     }
 }
